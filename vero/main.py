bce80ebebf2a8dcbe3dc0acf9d05528d717a56ad
Stefano Cereda <cereda.ste@gmail.com>
2020-02-20 19:16:51 +0100

    ok rdr;

--- vero/main.py
+++ vero/main.py
@@ -5,6 +5,7 @@ def read_file(filename):
     B number of different books
     L number of libraries
     D number of available days
+    S array of score given by books
     DS array length L: day to signup library j
     BD array length L: num books can be scanned in one day from library j
     BL dictionary of sets: set of books available in library key
@@ -15,8 +16,10 @@ def read_file(filename):
     # 1st line
     B, L, D = [int(x) for x in f[0].split(' ')]
 
+    S = np.array([int(x) for x in f[1].split(' ')])
+
     # L section
-    line = 1
+    line = 2
     DS = np.zeros(L)
     BD = np.zeros(L)
     BL = {}
@@ -25,12 +28,7 @@ def read_file(filename):
         line += 1
         BL[j] = set([int(x) for x in f[line].split(' ')])
         line += 1
-    return B, L, D, DS, BD, BL
-
-
-
-
-
+    return B, L, D, S, DS, BD, BL
 
 
 def write_file(solution, filename):
@@ -44,3 +42,32 @@ def scorer(solution):
 def check_constraint(solution):
     return True
 
+
+
+if __name__ == '__main__':
+    files = ['example']
+
+    algos = []
+    tot_score = 0
+
+    for f in files:
+        B, L, D, S, DS, BD, BL = read_file("input/{}.txt".format(f))
+        breakpoint()
+
+        scores = {}
+        solutions = {}
+        for alg in algos:
+            solution = alg(B, L, D, S, DS, BD, BL)
+            scores[alg] = scorer(solution)
+            solutions[alg] = solution
+            print("{:20s}{:50s}{}".format(f, alg.__name__, scores[alg]))
+
+        # search for best
+        best_alg = sorted(scores.items(), key=lambda x: -x[1])[0][0]
+        print("Using {}\n".format(best_alg.__name__))
+        tot_score += scores[best_alg]
+        solution = solutions[best_alg]
+        write_file(solution, "./output{}.txt".format(f))
+
+
+    print("Final score is {} ".format(tot_score))
